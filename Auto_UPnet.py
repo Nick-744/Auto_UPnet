d7a2fdb26c0082cf6b397a2af05bb9dc04c006e1
G. Nikolaos X <100804668+Nick-744@users.noreply.github.com>
2022-06-17 21:48:45 +0300

    Update Auto_UPnet.py

--- Auto_UPnet.py
+++ Auto_UPnet.py
@@ -1,50 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from warnings import filterwarnings
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-from os.path import getsize
-from os import getcwd
 from pushbullet import PushBullet
 from datetime import datetime
-from subprocess import CREATE_NO_WINDOW
-    
+
 def main():
 
-    new = 0
-    
+    output_memory = ""
+
     f = open("login_info.txt", "r")
     info = f.read()
     f.close()
-    info = info.split("\n")
     
+    info = info.split("\n")
     username = info[0][10:]
     password = info[1][10:]
     api_key = info[2][9:]
 
-    filterwarnings("ignore")
-
     submit_button = "[type=button]:not(:disabled), [type=reset]:not(:disabled), [type=submit]:not(:disabled), button:not(:disabled)"
     url = "https://mail1.upnet.gr/?_task=login"
 
-    
-    CHROMEDRIVER_PATH = str(getcwd()) + "\chromedriver.exe"
-    CHROME_PATH = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-    WINDOW_SIZE = "400,800"
+    service = Service(ChromeDriverManager().install())
 
-    service = Service(CHROMEDRIVER_PATH)
-    service.creationflags = CREATE_NO_WINDOW #Don't show cmd
+    options = Options()
+    options.add_argument("--headless") #Don't show browser
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless") #Don't show Chrome
-    chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-    chrome_options.binary_location = CHROME_PATH
-    
     while True:
 
         try:
-
-            driver = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH, chrome_options = chrome_options, service = service)
+            driver = webdriver.Chrome(options = options, service = service)
 
             try: driver.get(url)
             except:
@@ -52,39 +39,66 @@ def main():
                 f.write("The URL didn't load. | {}\n".format(str(datetime.now())[:19]))
                 f.close()
 
-            driver.find_element_by_name("_user").send_keys(username)
-            driver.find_element_by_name("_pass").send_keys(password)
-            driver.find_element_by_css_selector(submit_button).click()
+            driver.find_element(By.NAME, "_user").send_keys(username)
+            driver.find_element(By.NAME, "_pass").send_keys(password)
+            driver.find_element(By.CSS_SELECTOR, submit_button).click()
 
-            sleep(2) #Wait for page to load!
-
-            driver.get_screenshot_as_file("New_mail.png")
+            html = driver.page_source
+                    
             driver.close()
 
-            answer = getsize("New_mail.png")
+            all_messages_indexStart = html.index('<tbody><tr')
+            all_messages_indexEnd = html.index('</div>', all_messages_indexStart)
+            all_messages = html[all_messages_indexStart : all_messages_indexEnd]
 
-            if answer != 19388 and answer != new: #Check if New_mail.png size matches with no message size
+            messages = all_messages.split('class="message unread')
+            messages.pop(0)
 
-                new = answer #If you received a new mail, next time you will not be notified for the same mail!
+            new_messages_number = all_messages.count('class="msgicon status unread" title="Μη αναγνωσμένο "')
 
+            output = ""
+            
+            for information in range(len(messages)):
+                
+                if messages[information].count('Μη αναγνωσμένο') == 0: break
+                
+                mail_address_indexStart = messages[information].index('class="adr"><span title="') + 25
+                mail_address_indexEnd = messages[information].index('" class', mail_address_indexStart)
+                mail_address = messages[information][mail_address_indexStart : mail_address_indexEnd]
+
+                contact_address_indexStart = messages[information].index('Address') + 9
+                contact_address_indexEnd = messages[information].index('</span>', contact_address_indexStart)
+                contact_address = messages[information][contact_address_indexStart : contact_address_indexEnd]
+
+                date_indexStart = messages[information].index('class="date skip-on-drag">') + 26
+                date_indexEnd = messages[information].index('</span>', date_indexStart)
+                date = messages[information][date_indexStart : date_indexEnd]
+
+                title_indexStart = messages[information].index('long_subject_title') + 48
+                title_indexEnd = messages[information].index('</span>', title_indexStart)
+                title = messages[information][title_indexStart : title_indexEnd]
+                
+                output += "Τίτλος μυνήματος: "+title+"\nΑποστολέας: "+contact_address+" ["+mail_address+"]\nΗμερομηνία: "+date+"\n\n"
+
+            if output != output_memory:
+
+                output_memory = output
+                
                 try:
                     pb = PushBullet(api_key)
-                    push = pb.push_note("UPnet Webmail", "You have 1 new message!\n{}".format(str(datetime.now())[:19]))
+                    if new_messages_number > 1: push = pb.push_note("UPnet Webmail", "Έχετε {} νέα μυνήματα!\n\n{}Ημερομηνία συγχρονισμού: {}".format(new_messages_number, output, str(datetime.now())[:19]))
+                    elif new_messages_number == 1: push = pb.push_note("UPnet Webmail", "Έχετε 1 νέο μύνημα!\n\n{}Ημερομηνία συγχρονισμού: {}".format(output, str(datetime.now())[:19]))
 
-                    with open("New_mail.png", "rb") as pic:
-                        file_data = pb.upload_file(pic, "New_mail.png") #Send message to mobile device 
-                    push = pb.push_file(**file_data)
                 except:
                     f = open("login_errors.txt", "a", encoding = 'utf-8')
                     f.write("Con err. Wrong API_key. | {}\n\n".format(str(datetime.now())[:19]))
                     f.close()
-                
+
         except:
-            
             f = open("login_errors.txt", "a", encoding = 'utf-8')
             f.write("Couldn't execute program. | {}\n\n".format(str(datetime.now())[:19]))
             f.close()
 
-        sleep(300) #Δt waiting for new mail check!
+        sleep(180) #Δt waiting for new mail check!
 
 main()
